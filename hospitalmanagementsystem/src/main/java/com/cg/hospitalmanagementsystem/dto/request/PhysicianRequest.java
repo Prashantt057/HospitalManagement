1d64557e6403fcbd99639e723bd06b4f7f2bb64e
VishalVibe <136216047+VishalVibe@users.noreply.github.com>
2026-04-02 10:46:50 +0530

    added NureseRequest , PhysicianRequest , StaffLoginRequest and StaffRegister Request also added PhysicianPatientResponse

---
+++ hospitalmanagementsystem/src/main/java/com/cg/hospitalmanagementsystem/dto/request/PhysicianRequest.java
@@ -0,0 +1,12 @@
+package com.cg.hospitalmanagementsystem.dto.request;
+
+import lombok.Data;
+
+
+@Data
+public class PhysicianRequest {
+    private Integer id;
+    private String name;
+    private String position;
+}
+
